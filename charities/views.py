6e448659d01ce513a00a6d3a040aac0f397f14ad
Ali Rahimi <cerahimi74@gmail.com>
2023-07-06 16:29:25 +0330

    complete registering
    set pemission and set benefactor register

--- charities/views.py
+++ charities/views.py
@@ -1,3 +1,85 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import status, generics
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated, SAFE_METHODS
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from accounts.permissions import IsCharityOwner, IsBenefactor
+from charities.models import Task
+from charities.serializers import (
+    TaskSerializer, CharitySerializer, BenefactorSerializer
+)
+
+
+# this method is registered default user as a benefactor
+class BenefactorRegistration(APIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = BenefactorSerializer
+
+    def post(self, request):
+        serializer = BenefactorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(
+                data={'message': f'Benefactor {request.user.username} has been created successfully!'},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# this method is registered default user as a charity
+class CharityRegistration(APIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = CharitySerializer
+
+    def post(self, request):
+        serializer = CharitySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(
+                data={'message': f'Charity {request.user.username} has been created successfully!'},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class Tasks(generics.ListCreateAPIView):
+    serializer_class = TaskSerializer
+
+    def get_queryset(self):
+        return Task.objects.all_related_tasks_to_user(self.request.user)
+
+    def post(self, request, *args, **kwargs):
+        data = {
+            **request.data,
+            "charity_id": request.user.charity.id
+        }
+        serializer = self.serializer_class(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def get_permissions(self):
+        if self.request.method in SAFE_METHODS:
+            self.permission_classes = [IsAuthenticated, ]
+        else:
+            self.permission_classes = [IsCharityOwner, ]
+
+        return [permission() for permission in self.permission_classes]
+
+    def filter_queryset(self, queryset):
+        filter_lookups = {}
+        for name, value in Task.filtering_lookups:
+            param = self.request.GET.get(value)
+            if param:
+                filter_lookups[name] = param
+        exclude_lookups = {}
+        for name, value in Task.excluding_lookups:
+            param = self.request.GET.get(value)
+            if param:
+                exclude_lookups[name] = param
+
+        return queryset.filter(**filter_lookups).exclude(**exclude_lookups)
