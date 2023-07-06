428b1d1304c2595b3c7459a8b08238870a60ba63
Ali Rahimi <cerahimi74@gmail.com>
2023-07-06 16:20:49 +0330

    add rest api
    logout api and user register api

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+class LogoutAPIView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response(
+            data={'message': f'Bye {request.user.username}!'},
+            status=status.HTTP_204_NO_CONTENT
+        )
+
+
+# this method is used to register a user
+class UserRegistration(generics.CreateAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = UserSerializer
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                data={'message': f'User {serializer.data["username"]} has been created successfully!'},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
