9fc5c8ac6db64e426ad8f7fe83c1cb6fb4a7c7f9
Ali Rahimi <cerahimi74@gmail.com>
2023-07-06 14:39:13 +0330

    create admin for accounts
    add personal info, contact info, permissions and important dates

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+
+from accounts.models import User
+
+
+@admin.register(User)
+class UserAdmin(DefaultUserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active')
+    list_editable = ('is_staff', 'is_active')
+    list_filter = ('gender', 'is_staff', 'is_superuser', 'is_active')
+    sortable_by = ['username']
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (("Personal info"), {"fields": ("first_name", "last_name", "email", "gender", "age", "description")}), (
+            ("Contact info"), {"fields": ("phone", "address")}
+        ),
+        (
+            ("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
